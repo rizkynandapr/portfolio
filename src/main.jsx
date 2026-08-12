ec92088b08fbe57fef17dfc891be997a93105f7f
rizkynandapr <rizkynandapr@gmail.com>
2026-08-13 03:54:14 +0700

    feat: ink-on-paper design tokens and self-hosted fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import './styles/fonts.js';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
---
+++ src/styles/fonts.js
@@ -0,0 +1,6 @@
+// Self-hosted font faces. Imported once from main.jsx so no CDN request is made.
+import '@fontsource-variable/newsreader';
+import '@fontsource/ibm-plex-sans/400.css';
+import '@fontsource/ibm-plex-sans/500.css';
+import '@fontsource/ibm-plex-mono/400.css';
+import '@fontsource/ibm-plex-mono/500.css';
